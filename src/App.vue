457268b5d34db8340ab3d8048cf6ca3922c1441f
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-25 16:24:52 +0100

    [add] apiUrl

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <script>
+import axios from 'axios';
+//importa store
+import {store} from './assets/data/store';
 import Header from './components/Header.vue';
-import Main from './components/Main.vue';;
+import Main from './components/Main.vue';
 
   export default {
     components:{
         Header,
         Main,
       },
+      data(){
+        return{
+          store
+        }
+      },
 
-    
+      methods:{
+        getApi(){
+          axios.get(this.store.apiUrl)
+          // risposta
+          .then(result =>{
+            console.log(result.data);
+          })
+          //errore
+          .catch(error =>{
+            console.log(error);
+          })
+        }
+      },
+      mounted(){
+        this.getApi()
+      }
   }
 </script>
 
--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -3,5 +3,5 @@ import { reactive }  from 'vue'
 
 //  lo importo nei componenti che ne hanno bisogno
 export const store = reactive({
-
+  apiUrl: 'https://rickandmortyapi.com/api/character'
 })
